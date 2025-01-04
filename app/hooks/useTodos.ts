0b74a9da486be2151a104200aed399d22654770f
hnmwt <hnmwtr927@gmail.com>
2025-01-04 22:31:58 +0900

    fix

---
+++ app/hooks/useTodos.ts
@@ -0,0 +1,16 @@
+import { API_URL } from "@/constants/url";
+import useSWR from "swr";
+
+function fetcher(key: string) {
+    return fetch(key).then(res => res.json());
+}
+
+export const useTodos = () => {
+    const { data, isLoading, error, mutate } = useSWR(`${API_URL}/allTodos`, fetcher);
+    return {
+        todos: data,
+        isLoading,
+        error,
+        mutate,
+    }
+}
